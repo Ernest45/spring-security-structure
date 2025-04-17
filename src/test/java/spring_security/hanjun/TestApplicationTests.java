8be67222fb7c21a0382ff095891a0cc36265a5bf
Ernest45 <gkgkswns71@gmail.com>
2025-04-17 23:53:42 +0900

    스프링 내부 구조 알아보기 프로젝트

---
+++ src/test/java/spring_security/hanjun/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_security.hanjun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
